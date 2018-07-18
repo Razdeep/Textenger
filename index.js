e25edaaf7e272fb43fff4e122912f103e93375a3
razdeep <rrajdeeproychowdhury@gmail.com>
2018-07-18 09:52:50 +0530

    Look and feel added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 express=require('express')
 path=require('path')
 app=express()
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/public/home.html')
