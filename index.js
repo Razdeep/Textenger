3dc2b164198ab3458dae7d061ae9ff932a449ad4
razdeep <rrajdeeproychowdhury@gmail.com>
2018-07-19 11:09:12 +0530

    body-parser package included and applied

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 express=require('express')
 path=require('path')
 dotenv=require('dotenv')
+bodyParser=require('body-parser')
 dotenv.config()
 client=require('twilio')(process.env.TWILIO_SID,process.env.TWILIO_TOKEN)
 app=express()
 app.use(express.static(path.join(__dirname, 'public')))
 // console.log(process.env.TWILIO_SID)
 // console.log(process.env.TWILIO_TOKEN)
-
+app.use(bodyParser.urlencoded({extended: true}))
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/public/home.html')
 })
@@ -15,13 +16,14 @@ app.post('/',function(req,res){
     client.messages.create({
         to: process.env.MY_PHONE_NUMBER,
         from: process.env.MY_TWILIO_NUMBER,
-        body: 'testing testing'
+        body: req.body.message
     },function(err,data){
         if(err)
             console.log(err)
         console.log(data)
     }).then((messsage) => console.log(message.sid))
-    res.send('Submitted successfully')
+    console.log('RRC : Sending MESSAGE SENT text')
+    res.send('Message sent... ')
 })
 
 app.listen((process.env.PORT||1998),function(err){
